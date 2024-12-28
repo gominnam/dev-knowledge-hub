8db16c54aca785aefe2a12f4f38b26e7e3025425
gominnam <kosok03@naver.com>
2024-12-28 16:50:12 +0900

    edit class Dollar

--- src/main/java/com/hello/tdd/money/Dollar.java
+++ src/main/java/com/hello/tdd/money/Dollar.java
@@ -4,10 +4,10 @@ public class Dollar {
     int amount;
 
     Dollar(int amount){
-
+        this.amount = amount;
     }
 
     void times(int multiplier){
-
+        amount *= multiplier;
     }
 }
