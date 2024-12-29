20a22372fc3650d27c655726670412bd9bc88709
gominnam <kosok03@naver.com>
2024-12-29 19:24:35 +0900

    Add equals method and test

--- src/test/java/com/hello/tdd/money/MoneyTest.java
+++ src/test/java/com/hello/tdd/money/MoneyTest.java
@@ -2,7 +2,7 @@ package com.hello.tdd.money;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
 
@@ -13,5 +13,11 @@ public class MoneyTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6))); // 삼각측량
     }
 }
