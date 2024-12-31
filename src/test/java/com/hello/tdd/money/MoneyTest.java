7bbb86ed9897bd831c94760c63527bcbd23d8702
gominnam <kosok03@naver.com>
2024-12-31 20:25:03 +0900

    refactoring Bank, Expression, Sum, Money

--- src/test/java/com/hello/tdd/money/MoneyTest.java
+++ src/test/java/com/hello/tdd/money/MoneyTest.java
@@ -41,4 +41,28 @@ public class MoneyTest {
         Money reduced = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(10), reduced);
     }
+
+    @Test
+    public void testPlusReturnsSum() {
+        Money five = Money.dollar(5);
+        Expression result = five.plus(five);
+        Sum sum = (Sum)result;
+        assertEquals(five, sum.augend); // augend: 피 가수
+        assertEquals(five, sum.addend); // addend: 가수
+    }
+
+    @Test
+    public void testReduceSum() {
+        Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank = new Bank();
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(7), result);
+    }
+
+    @Test
+    public void testReduceMoney() {
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
 }
